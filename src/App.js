c6d08ee75403be41e1d089175c7269963ee94f0a
DESKTOP-SFN13LD\ariff <ariffaisal118@gmail.com>
2024-02-05 02:18:30 +0700

    update link bootstrap

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css'
+import 'href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css'
 
 function App() {
   function buttonLink(){
